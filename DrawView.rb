c8f7ff80ce5e9c85813b2b81acd1cdd8b69552fc
whoamix <mfierek@protonmail.ch>
2019-01-14 20:32:06 +0100

    Extracted flag, index variable for readbility

--- DrawView.rb
+++ DrawView.rb
@@ -19,12 +19,14 @@ def PrintToTerminal(ants, xSize, ySize, allAntRoles)
 end
 
 def CheckAntPos(ants, x, y)
+    flag = false
+    index = 0
     for i in 0..(ants.length - 1)
         if (ants[i].GetPos() == [x, y])
             return true, i
         end
     end
-    return false, 0
+    return flag, index
 end
 
 def GenetarteNewBorderAnt(xSize, ySize)
@@ -59,33 +61,6 @@ def SimulateAnts(tabXSize, tabYSize, numberOfAnts, antMovementIterations, allAnt
     end
     print "\033[#{tabYSize}B"
 end
-
-class String
-    def black;          "\e[30m#{self}\e[0m" end
-    def red;            "\e[31m#{self}\e[0m" end
-    def green;          "\e[32m#{self}\e[0m" end
-    def brown;          "\e[33m#{self}\e[0m" end
-    def blue;           "\e[34m#{self}\e[0m" end
-    def magenta;        "\e[35m#{self}\e[0m" end
-    def cyan;           "\e[36m#{self}\e[0m" end
-    def gray;           "\e[37m#{self}\e[0m" end
-    
-    def bg_black;       "\e[40m#{self}\e[0m" end
-    def bg_red;         "\e[41m#{self}\e[0m" end
-    def bg_green;       "\e[42m#{self}\e[0m" end
-    def bg_brown;       "\e[43m#{self}\e[0m" end
-    def bg_blue;        "\e[44m#{self}\e[0m" end
-    def bg_magenta;     "\e[45m#{self}\e[0m" end
-    def bg_cyan;        "\e[46m#{self}\e[0m" end
-    def bg_gray;        "\e[47m#{self}\e[0m" end
-    
-    def bold;           "\e[1m#{self}\e[22m" end
-    def italic;         "\e[3m#{self}\e[23m" end
-    def underline;      "\e[4m#{self}\e[24m" end
-    def blink;          "\e[5m#{self}\e[25m" end
-    def reverse_color;  "\e[7m#{self}\e[27m" end
-    end
-
 
 tabXSize = 40;
 tabYSize = 15;
