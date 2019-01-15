9b7310255e89c72fa77b204b42b20a92c89571d7
bartlej <bjarzembinski@yahoo.pl>
2019-01-15 03:39:21 +0100

    Change Function Declaration - Remove Parameter

--- Mrowisko/DrawView.rb
+++ Mrowisko/DrawView.rb
@@ -2,14 +2,14 @@ require './Ant'
 
 CLEARLINE = "\033[K"
 CSI = "\033" #CONTROL_SEQUENCE_INTRODUCER 
+ALL_ANTS_ROLES = {"Warrior" => 31, "Gatherer" => 34, "Worker" => 27}
 
 tab_x_size = 40
 tab_y_size = 15
 number_of_ants = 15
 ant_movement_iteration = 20
-all_ant_roles = {"Warrior" => 31, "Gatherer" => 34, "Worker" => 27}
 
-def print_to_treminal(ants, x_size, y_size, all_ant_roles)
+def print_to_treminal(ants, x_size, y_size)
     position = y_size
     ant_symbol = "\u{1F41C}"
     for y in 0..(y_size - 1)
@@ -17,7 +17,7 @@ def print_to_treminal(ants, x_size, y_size, all_ant_roles)
         for x in 0..(x_size - 1)
             flag, index = check_ant_position(ants, x, y)
             if (flag)
-                print "\e[#{all_ant_roles[ants[index].get_role()]}m#{ant_symbol}\e[0m"
+                print "\e[#{ALL_ANTS_ROLES[ants[index].get_role()]}m#{ant_symbol}\e[0m"
             else
                 print " "
             end
@@ -51,14 +51,14 @@ def generate_new_border_ant(x_size, y_size)
     return Ant.new(rand_x, rand_y)
 end
 
-def simulate_ants(tab_x_size, tab_y_size, number_of_ants, ant_movement_iteration, all_ant_roles)
+def simulate_ants(tab_x_size, tab_y_size, number_of_ants, ant_movement_iteration)
     ant_tab = []
     for i in 1..number_of_ants
-        ant = Ant.new(rand(tab_x_size), rand(tab_y_size), all_ant_roles.keys, all_ant_roles.keys[rand(all_ant_roles.length)])
+        ant = Ant.new(rand(tab_x_size), rand(tab_y_size), ALL_ANTS_ROLES.keys, ALL_ANTS_ROLES.keys[rand(ALL_ANTS_ROLES.length)])
         ant_tab.push(ant)
     end
     for i in 1..ant_movement_iteration
-        print_to_treminal(ant_tab, tab_x_size, tab_y_size, all_ant_roles)
+        print_to_treminal(ant_tab, tab_x_size, tab_y_size)
         for j in 0..(ant_tab.length - 1)
             ant_tab[j].move_in_random_direction()
             if (ant_tab[j].get_position()[0] >= tab_x_size || ant_tab[j].get_position()[0] <= 0) ||
@@ -71,4 +71,4 @@ def simulate_ants(tab_x_size, tab_y_size, number_of_ants, ant_movement_iteration
     print "#{CSI}[#{tab_y_size}B"
 end
 
-simulate_ants(tab_x_size, tab_y_size, number_of_ants, ant_movement_iteration, all_ant_roles)
+simulate_ants(tab_x_size, tab_y_size, number_of_ants, ant_movement_iteration)
